5f67d166066140f0e8da8875a869a67d0a850ab9
LanDev01 <alnjr0001@outlook.com>
2025-02-05 17:44:30 -0300

    Feat: add support for pasting images via clipboard

--- scripts/script.js
+++ scripts/script.js
@@ -19,3 +19,20 @@ dropArea.addEventListener("drop", (e) => {
   inputFile.files = e.dataTransfer.files;
   uploadImage();
 })
+
+document.addEventListener("paste", (e) => {
+  const items = e.clipboardData.items;
+  for (let i = 0; i < items.length; i++) {
+    if (items[i].type.indexOf("image") !== -1) {
+      const blob = items[i].getAsFile();
+      const imgLink = URL.createObjectURL(blob);
+      imageView.style.backgroundImage = `url(${imgLink})`;
+      imageView.textContent = "";
+      imageView.style.border = 0;
+      
+      const dataTransfer = new DataTransfer();
+      dataTransfer.items.add(blob);
+      inputFile.files = dataTransfer.files;
+    }
+  }
+});
